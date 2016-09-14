547449964f8b72f20be01f3b65b85c669ba61af0
Atul Bhouraskar <atul@yescourse.com>
2016-09-14 07:44:20 -0400

    Make theme.css compile compressed by default

--- helium/src/scss/config.rb
+++ helium/src/scss/config.rb
@@ -13,13 +13,13 @@ javascripts_dir = "../../js"
 http_images_path = "../images/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed  # :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # Workaround for compiling in vagrant devel-env on windows hosts using vboxsf.
