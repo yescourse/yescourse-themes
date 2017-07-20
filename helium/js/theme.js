cb569696d4670867de5e5b676015e96aa57773bb
Atul Bhouraskar <atul@yescourse.com>
2017-07-20 10:13:52 +1000

    Update aside styling for mobile

--- helium/js/theme.js
+++ helium/js/theme.js
@@ -1,13 +1,29 @@
 (function() {
     var root_element = document.getElementById('yc-root');
+    var user_menu = root_element.querySelector('nav.user');
     root_element.addEventListener('click', function(e) {
         var target = e.target;
         if (target.tagName === 'NAV' && target.classList.contains('user')) {
-            target.classList.toggle('active');
+            if (!user_menu) {
+                user_menu = target;
+            }
+            user_menu.classList.toggle('active');
+            root_element.classList.toggle('show-user-menu');
         }
         else if (target.tagName === 'A' &&
                  target.classList.contains('show-main-menu')) {
             root_element.classList.toggle('show-main-menu');
+        }
+        else if (target.id === 'veil') {
+            if (root_element.classList.contains('show-main-menu')) {
+                root_element.classList.remove('show-main-menu');
+            }
+            if (root_element.classList.contains('show-user-menu')) {
+                root_element.classList.remove('show-user-menu');
+                if (user_menu) {
+                    user_menu.classList.remove('active');
+                }
+            }
         }
     });
 })();
