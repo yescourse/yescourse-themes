bf498d8fba0430d4f25574a4c09e3f4dfd9a4fc5
Atul Bhouraskar <atul@yescourse.com>
2022-03-18 10:17:05 +1100

    #6168: Upgrade to dart-sass

--- helium/src/scss/compile.sh
+++ helium/src/scss/compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-SCRIPTDIR=`dirname "$(readlink -f "$0")"`
+set -e
 
-pushd ${SCRIPTDIR}
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+pushd "${SCRIPT_DIR}"
 
 export SASS_PATH=".:../../.."
 
@@ -10,35 +12,25 @@ CSS_PATH=../../css
 
 options="--style compressed"
 
-# FIXME: once sassc is installed in the VM remove this and just use sassc
-if [[ $(type -P sassc) ]]
-then
-    compiler=sassc
-else
-    compiler=scss
-fi
+compiler=sass
 
 if [ "$1" = "--expanded" ]
 then
-    options="--style expanded --line-numbers"
+    options="--style expanded"
 fi
 
-if [ ${compiler} = "sassc" ]
-then
-    # sassc does not look at the SASS_PATH env, rather needs to be passed in
-    options="$options --load-path $SASS_PATH"
-fi
+# path where relative imports can be found (assumes run from scss/ dir)
+options="$options -I $SCRIPT_DIR"
 
 function compile()
 {
 	echo "Compiling $1 to ${CSS_PATH}/$2 using ${compiler}"
 	mkdir -p ${CSS_PATH}
-	${compiler} ${options} $1 ${CSS_PATH}/$2
+	# shellcheck disable=SC2086
+	${compiler} ${options} "$1" ${CSS_PATH}/"$2"
 }
 
-for file in theme.scss
-do
-	compile ${file} ${file%.scss}.css
-done
+file=theme.scss
+compile ${file} ${file%.scss}.css
 
 popd
